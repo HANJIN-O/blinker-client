66cf1080c4b7e24211e0fdc7bd69721ef06181aa
HAN-O <zeros0623@gmail.com>
2019-09-02 14:41:30 +0900

    reinit

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function App() {
   return (
-    <div className='App'>
-      <div id='_wrapper'>
-        <div id='_content'>
-          <video id='_webcam' playsInline style={{ display: "none" }}></video>
-          <canvas id='_imageData'></canvas>
-          <canvas id='_faceSub'></canvas>
-          <canvas id='_t3d'></canvas>
-          <canvas id='_f3d'></canvas>
-          <canvas id='_drawing'></canvas>
-          <div id='_progressBar'></div>
+    <div className="App">
+      <div id="_wrapper">
+        <div id="_content">
+          <video id="_webcam" playsInline style={{ display: "none" }}></video>
+          <canvas id="_imageData"></canvas>
+          <canvas id="_faceSub"></canvas>
+          <canvas id="_t3d"></canvas>
+          <canvas id="_f3d"></canvas>
+          <canvas id="_drawing"></canvas>
+          <div id="_progressBar"></div>
         </div>
       </div>
     </div>
