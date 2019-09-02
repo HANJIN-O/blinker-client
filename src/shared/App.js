28b077d34a928ef40900d1ce3ba62888aa16ad86
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-09-02 16:25:57 +0900

    implemented game script in App.js

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <div id="_wrapper">
-        <div id="_content">
-          <video id="_webcam" playsInline style={{ display: "none" }}></video>
-          <canvas id="_imageData"></canvas>
-          <canvas id="_faceSub"></canvas>
-          <canvas id="_t3d"></canvas>
-          <canvas id="_f3d"></canvas>
-          <canvas id="_drawing"></canvas>
-          <div id="_progressBar"></div>
+export default class App extends Component {
+  constructor () {
+    super ();
+    this.state = {score: 0};
+  }
+
+  componentWillMount () {
+    console.log ('componentWillMount is called');
+    const script = document.createElement ('script');
+    script.src = '/games/flappyBird.js';
+    const scriptCdn = document.createElement ('script');
+    scriptCdn.src = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js';
+    document.body.appendChild (script);
+    document.body.appendChild (scriptCdn);
+  }
+
+  render () {
+    console.log (this.state);
+    return (
+      <div className="App">
+        <div id="_wrapper">
+          <div id="_content">
+            <video id="_webcam" playsInline style={{display: 'none'}} />
+            <canvas id="_imageData" />
+            <canvas id="_faceSub" />
+            <canvas id="_t3d" />
+            <canvas id="_f3d" />
+            <canvas id="_drawing" />
+            <div id="_progressBar" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
