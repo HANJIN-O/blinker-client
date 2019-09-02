c91fe4f4ebb889dbaa65c2767563ec1691fbd428
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-09-02 14:59:06 +0900

    modified dev env

--- public/js/examples/face_tracking/blink_detection.js
+++ public/js/examples/face_tracking/blink_detection.js
@@ -1,123 +1,154 @@
-(function exampleCode() {
-	"use strict";
+(function exampleCode () {
+  'use strict';
+  brfv4Example.initCurrentExample = function (brfManager, resolution) {
+    brfManager.init (resolution, resolution, brfv4Example.appId);
+  };
 
-	brfv4Example.initCurrentExample = function(brfManager, resolution) {
-		brfManager.init(resolution, resolution, brfv4Example.appId);
-	};
+  brfv4Example.updateCurrentExample = function (brfManager, imageData, draw) {
+    brfManager.update (imageData);
 
-	brfv4Example.updateCurrentExample = function(brfManager, imageData, draw) {
+    draw.clear ();
 
-		brfManager.update(imageData);
+    // Face detection results: a rough rectangle used to start the face tracking.
 
-		draw.clear();
+    draw.drawRects (
+      brfManager.getAllDetectedFaces (),
+      false,
+      1.0,
+      0x00a1ff,
+      0.5
+    );
+    draw.drawRects (
+      brfManager.getMergedDetectedFaces (),
+      false,
+      2.0,
+      0xffd200,
+      1.0
+    );
 
-		// Face detection results: a rough rectangle used to start the face tracking.
+    var faces = brfManager.getFaces (); // default: one face, only one element in that array.
 
-		draw.drawRects(brfManager.getAllDetectedFaces(),	false, 1.0, 0x00a1ff, 0.5);
-		draw.drawRects(brfManager.getMergedDetectedFaces(),	false, 2.0, 0xffd200, 1.0);
+    for (var i = 0; i < faces.length; i++) {
+      var face = faces[i];
 
-		var faces = brfManager.getFaces(); // default: one face, only one element in that array.
+      if (face.state === brfv4.BRFState.FACE_TRACKING) {
+        // simple blink detection
 
-		for(var i = 0; i < faces.length; i++) {
+        // A simple approach with quite a lot false positives. Fast movement can't be
+        // handled properly. This code is quite good when it comes to
+        // staring contest apps though.
 
-			var face = faces[i];
+        // It basically compares the old positions of the eye points to the current ones.
+        // If rapid movement of the current points was detected it's considered a blink.
 
-			if(face.state === brfv4.BRFState.FACE_TRACKING) {
+        var v = face.vertices;
 
-				// simple blink detection
+        if (_oldFaceShapeVertices.length === 0) storeFaceShapeVertices (v);
 
-				// A simple approach with quite a lot false positives. Fast movement can't be
-				// handled properly. This code is quite good when it comes to
-				// staring contest apps though.
+        var k, l, yLE, yRE;
 
-				// It basically compares the old positions of the eye points to the current ones.
-				// If rapid movement of the current points was detected it's considered a blink.
+        // Left eye movement (y)
 
-				var v = face.vertices;
+        for ((k = 36), (l = 41), (yLE = 0); k <= l; k++) {
+          yLE += v[k * 2 + 1] - _oldFaceShapeVertices[k * 2 + 1];
+        }
+        yLE /= 18; //default 6
 
-				if(_oldFaceShapeVertices.length === 0) storeFaceShapeVertices(v);
+        // Right eye movement (y)
 
-				var k, l, yLE, yRE;
+        for ((k = 42), (l = 47), (yRE = 0); k <= l; k++) {
+          yRE += v[k * 2 + 1] - _oldFaceShapeVertices[k * 2 + 1];
+        }
 
-				// Left eye movement (y)
+        yRE /= 18; //default 6
 
-				for(k = 36, l = 41, yLE = 0; k <= l; k++) {
-					yLE += v[k * 2 + 1] - _oldFaceShapeVertices[k * 2 + 1];
-				}
-				yLE /= 18; //default 6
+        var yN = 0;
 
-				// Right eye movement (y)
+        // Compare to overall movement (nose y)
 
-				for(k = 42, l = 47, yRE = 0; k <= l; k++) {
-					yRE += v[k * 2 + 1] - _oldFaceShapeVertices[k * 2 + 1];
-				}
+        yN += v[27 * 2 + 1] - _oldFaceShapeVertices[27 * 2 + 1];
+        yN += v[28 * 2 + 1] - _oldFaceShapeVertices[28 * 2 + 1];
+        yN += v[29 * 2 + 1] - _oldFaceShapeVertices[29 * 2 + 1];
+        yN += v[30 * 2 + 1] - _oldFaceShapeVertices[30 * 2 + 1];
+        yN /= 16; //default 4
 
-				yRE /= 18; //default 6
+        var blinkRatio = Math.abs ((yLE + yRE) / yN);
 
-				var yN = 0;
+        if (blinkRatio > 12 && (yLE > 0.4 || yRE > 0.4)) {
+          console.log (
+            'blink ' +
+              blinkRatio.toFixed (2) +
+              ' ' +
+              yLE.toFixed (2) +
+              ' ' +
+              yRE.toFixed (2) +
+              ' ' +
+              yN.toFixed (2)
+          );
 
-				// Compare to overall movement (nose y)
+          blink ();
+        }
 
-				yN += v[27 * 2 + 1] - _oldFaceShapeVertices[27 * 2 + 1];
-				yN += v[28 * 2 + 1] - _oldFaceShapeVertices[28 * 2 + 1];
-				yN += v[29 * 2 + 1] - _oldFaceShapeVertices[29 * 2 + 1];
-				yN += v[30 * 2 + 1] - _oldFaceShapeVertices[30 * 2 + 1];
-				yN /= 16; //default 4
+        // Let the color of the shape show whether you blinked.
 
-				var blinkRatio = Math.abs((yLE + yRE) / yN);
+        var color = 0x00a0ff;
 
-				if((blinkRatio > 12 && (yLE > 0.4 || yRE > 0.4))) {
-					console.log("blink " + blinkRatio.toFixed(2) + " " + yLE.toFixed(2) + " " +
-						yRE.toFixed(2) + " " + yN.toFixed(2));
+        if (_blinked) {
+          color = 0xffd200;
+        }
 
-					blink();
-				}
+        // Face Tracking results: 68 facial feature points.
 
-				// Let the color of the shape show whether you blinked.
+        draw.drawTriangles (
+          face.vertices,
+          face.triangles,
+          false,
+          1.0,
+          color,
+          0.4
+        );
+        draw.drawVertices (face.vertices, 2.0, false, color, 0.4);
 
-				var color = 0x00a0ff;
+        brfv4Example.dom.updateHeadline (
+          'BRFv4 - advanced - face tracking - simple blink' +
+            'detection.\nDetects an eye  blink: ' +
+            (_blinked ? 'Yes' : 'No')
+        );
 
-				if(_blinked) {
-					color = 0xffd200;
-				}
+        storeFaceShapeVertices (v);
+      }
+    }
+  };
 
-				// Face Tracking results: 68 facial feature points.
+  function blink () {
+    _blinked = true;
+    bird.jump ();
 
-				draw.drawTriangles(	face.vertices, face.triangles, false, 1.0, color, 0.4);
-				draw.drawVertices(	face.vertices, 2.0, false, color, 0.4);
+    if (_timeOut > -1) {
+      clearTimeout (_timeOut);
+    }
+    console.log ('blink!');
+    _timeOut = setTimeout (resetBlink, 0);
+  }
 
-				brfv4Example.dom.updateHeadline("BRFv4 - advanced - face tracking - simple blink" +
-					"detection.\nDetects an eye  blink: " + (_blinked ? "Yes" : "No"));
+  function resetBlink () {
+    _blinked = false;
+  }
 
-				storeFaceShapeVertices(v);
-			}
-		}
-	};
+  function storeFaceShapeVertices (vertices) {
+    for (var i = 0, l = vertices.length; i < l; i++) {
+      _oldFaceShapeVertices[i] = vertices[i];
+    }
+  }
 
-	function blink() {
-		_blinked = true;
+  var _oldFaceShapeVertices = [];
+  var _blinked = false;
+  var _timeOut = -1;
 
-		if(_timeOut > -1) { clearTimeout(_timeOut); }
-		console.log('blink!')
-		_timeOut = setTimeout(resetBlink, 0);
-	}
+  brfv4Example.dom.updateHeadline (
+    'BRFv4 - advanced - face tracking - simple blink detection.\n' +
+      'Detects a blink of the eyes: '
+  );
 
-	function resetBlink() {
-		_blinked = false;
-	}
-
-	function storeFaceShapeVertices(vertices) {
-		for(var i = 0, l = vertices.length; i < l; i++) {
-			_oldFaceShapeVertices[i] = vertices[i];
-		}
-	}
-
-	var _oldFaceShapeVertices = [];
-	var _blinked		= false;
-	var _timeOut		= -1;
-
-	brfv4Example.dom.updateHeadline("BRFv4 - advanced - face tracking - simple blink detection.\n" +
-		"Detects a blink of the eyes: ");
-
-	brfv4Example.dom.updateCodeSnippet(exampleCode + "");
-})();
+  brfv4Example.dom.updateCodeSnippet (exampleCode + '');
+}) ();
