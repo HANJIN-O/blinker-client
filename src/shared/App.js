62b6ec2681fc09a239466564801c41262c9c9804
HAN-O <zeros0623@gmail.com>
2019-09-02 11:27:21 +0900

    init

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../stylesheet/App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='_wrapper'>
+        <div id='_content'>
+          <video id='_webcam' playsInline style={{ display: "none" }}></video>
+          <canvas id='_imageData'></canvas>
+          <canvas id='_faceSub'></canvas>
+          <canvas id='_t3d'></canvas>
+          <canvas id='_f3d'></canvas>
+          <canvas id='_drawing'></canvas>
+          <div id='_progressBar'></div>
+        </div>
+      </div>
     </div>
   );
 }
