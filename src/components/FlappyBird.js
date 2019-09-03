d84081b95822f39057629222285e33401f2068a9
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-09-03 17:38:23 +0900

    added function to execute promise

--- src/components/FlappyBird.js
+++ src/components/FlappyBird.js
@@ -2,23 +2,28 @@ import React, { Component } from "react";
 import documentReady from "document-ready-promise";
 import cdnPromise from "cdn-promise";
 
-Promise.all([
-  cdnPromise("/js/BRFv4Demo.js"),
-  cdnPromise("/js/libs/createjs/preloadjs.min.js")
-])
-  .then(() => window.brfv4Example.start())
-  .then(() => {
-    return Promise.all([
-      cdnPromise("https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js"),
-      cdnPromise("/games/flappyBird.js"),
-      documentReady()
-    ]);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+function loadScript() {
+  return Promise.all([
+    cdnPromise("/js/BRFv4Demo.js"),
+    cdnPromise("/js/libs/createjs/preloadjs.min.js")
+  ])
+    .then(() => window.brfv4Example.start())
+    .then(() => {
+      return Promise.all([
+        cdnPromise("https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js"),
+        cdnPromise("/games/flappyBird.js"),
+        documentReady()
+      ]);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
 
 export default class FlappyBird extends Component {
+  componentDidMount() {
+    loadScript();
+  }
   render() {
     return <div>flappybird</div>;
   }
