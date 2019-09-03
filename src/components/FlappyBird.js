4796a2456af9cc70015d3ab82ca2047aa76297e1
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-09-03 16:12:59 +0900

    implementing async await for scripts

--- src/components/FlappyBird.js
+++ src/components/FlappyBird.js
@@ -1,18 +1,34 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+
+function loadScripts () {
+  const brfv4demo = document.createElement ('script');
+  brfv4demo.src = '/js/BRFv4Demo.js';
+  document.body.appendChild (brfv4demo);
+
+  const preloadjs = document.createElement ('script');
+  preloadjs.src = '/js/libs/createjs/preloadjs.min.js';
+  document.body.appendChild (preloadjs);
+
+  const gameScriptCdn = document.createElement ('script');
+  gameScriptCdn.src =
+    'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js';
+  document.body.appendChild (gameScriptCdn);
+
+  const gameScript = document.createElement ('script');
+  gameScript.src = '/games/flappyBird.js';
+  document.body.appendChild (gameScript);
+}
 
 export default class FlappyBird extends Component {
-  componentDidMount() {
-    const scriptCdn = document.createElement("script");
-    scriptCdn.src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js";
-    document.body.appendChild(scriptCdn);
-    window.loadFlappyBird();
+  do_load = async () => {
+    loadScripts ();
+    setTimeout (() => {
+      window.brfv4Example.start ();
+    }, 3000);
+  };
 
-    const script = document.createElement("script");
-    script.src = "/games/flappyBird.js";
-    document.body.appendChild(script);
-  }
-
-  render() {
+  render () {
+    this.do_load ();
     return <div>flappybird</div>;
   }
 }
