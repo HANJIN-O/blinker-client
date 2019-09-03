22bc645ebf4d27d8cfa1fc91ad000590b25a419c
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-09-03 17:25:27 +0900

    implemented promise

--- src/components/FlappyBird.js
+++ src/components/FlappyBird.js
@@ -1,34 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import documentReady from "document-ready-promise";
+import cdnPromise from "cdn-promise";
 
-function loadScripts () {
-  const brfv4demo = document.createElement ('script');
-  brfv4demo.src = '/js/BRFv4Demo.js';
-  document.body.appendChild (brfv4demo);
-
-  const preloadjs = document.createElement ('script');
-  preloadjs.src = '/js/libs/createjs/preloadjs.min.js';
-  document.body.appendChild (preloadjs);
-
-  const gameScriptCdn = document.createElement ('script');
-  gameScriptCdn.src =
-    'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js';
-  document.body.appendChild (gameScriptCdn);
-
-  const gameScript = document.createElement ('script');
-  gameScript.src = '/games/flappyBird.js';
-  document.body.appendChild (gameScript);
-}
+Promise.all([
+  cdnPromise("/js/BRFv4Demo.js"),
+  cdnPromise("/js/libs/createjs/preloadjs.min.js")
+])
+  .then(() => window.brfv4Example.start())
+  .then(() => {
+    return Promise.all([
+      cdnPromise("https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js"),
+      cdnPromise("/games/flappyBird.js"),
+      documentReady()
+    ]);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 export default class FlappyBird extends Component {
-  do_load = async () => {
-    loadScripts ();
-    setTimeout (() => {
-      window.brfv4Example.start ();
-    }, 3000);
-  };
-
-  render () {
-    this.do_load ();
+  render() {
     return <div>flappybird</div>;
   }
 }
