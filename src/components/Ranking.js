d283047efde0c5520b45435ae0baf8202ba60f86
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2019-09-05 11:02:41 +0900

    make rankingCompo

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
-const Ranking = () => {
-  return (
-    <div>
-      <Link to={`/home/ranking`}>
-        <button>ranking</button>
-      </Link>
-    </div>
-  );
-};
+export default class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = { userInfo: [{ username: "이찬행", score: 50 }] }; // dummy
+  }
 
-export default Ranking;
+  //* componentWillMount DOM manipulation X
+  //* serverSide rendering => componentWillMount
+  componentDidMount() {
+    axios({
+      url: "/rank/home",
+      method: "get",
+      params: {
+        gameName: "flappyBird"
+      },
+      data: {
+        username: "",
+        score: ""
+      }
+    })
+      .then(res => {
+        this.setState({ userInfo: res.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    // map 핑 한걸 뽑고
+
+    return (
+      <div>
+        <h2>Blinker</h2>
+        <h3>User Ranking</h3>
+        <div>
+          {this.state.userInfo.map((userdata, i) => (
+            <div className="userRankingInfo">
+              {" "}
+              <span key={i}>{userdata.username}</span>{" "}
+              <span>{userdata.score}</span>{" "}
+            </div>
+          ))}
+        </div>
+        <Link to={"/Home/flappybird"}>
+          <button>Go back to Game</button>
+        </Link>
+      </div>
+    );
+  }
+}
