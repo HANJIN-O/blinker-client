03b1e1df50aa977ecfb8b727882aaf6aed773c6f
yobs0814 <yobs0814@gmail.com>
2019-09-06 17:54:50 +0900

    ranking css 하고, singin, singnup 코딩 추가하였씁니다.

--- src/components/FlappyBirdGame.js
+++ src/components/FlappyBirdGame.js
@@ -22,18 +22,18 @@ class FlappyBirdGame extends Component {
   // 그래서 게임이 끝났을때 서버로 요청을 보낼겁니당.
   post() {
     let url = `http://ec2-13-209-35-43.ap-northeast-2.compute.amazonaws.com`;
-    console.log('포스트!!!!');
+    console.log("포스트!!!!");
     axios({
       url: `${url}/score`,
       method: "post",
       data: {
-        username: '곽두팔', // userId는 signin할때 props로 내려줘야겠죠.
+        username: "곽두팔", // userId는 signin할때 props로 내려줘야겠죠.
         gameId: 1, // 현재는 게임이 한개라서 하드 코딩하는데 이 gameId는 어디서 관리할지 모르겠네요.
         score: this.state.score
       }
     })
       .then(res => {
-        console.log('스코어보냈어요',res); // score를 서버에 HTTP POST 요청을 보낸후에 할 작업을 여기에 넣어주세요.
+        console.log("스코어보냈어요", res); // score를 서버에 HTTP POST 요청을 보낸후에 할 작업을 여기에 넣어주세요.
       })
       .catch(error => {
         console.log(error);
