3a6359b51a259d23273a420496e38ecda69d4da7
eLt <48753593+CgodL@users.noreply.github.com>
2019-09-10 15:57:20 +0900

    fin style NotFound (#40)

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../stylesheet/NotFound.css";
 
 export default class NotFound extends Component {
   render() {
     return (
       <div className="not-found">
+        <div className="sorry">SORRY PAGE NOT FOUND</div>
+        <div className="error">404</div>
+        <h1 className="ERROR"> ERROR </h1>
         <Link to="/">Go to Home</Link>
-        <h1 className="title">잘못된 접근 !!!!!</h1>
       </div>
     );
   }
